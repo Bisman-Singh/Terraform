b42d8e5f4e88742408076a606ca0b96c06f521fc
Bisman Singh <133761388+Bisman-Singh@users.noreply.github.com>
2024-07-30 13:58:11 +0530

    Update aws-vpc-setup.tf

--- aws-vpc-setup.tf
+++ aws-vpc-setup.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"  # Replace with your desired VPC CIDR block
+  cidr_block = "10.0.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
   tags = {
@@ -13,8 +13,8 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "public" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"  # Replace with your desired public subnet CIDR block
-  availability_zone = "us-west-2a"  # Replace with your desired availability zone
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
   map_public_ip_on_launch = true
   tags = {
     Name = "PublicSubnet"
@@ -23,8 +23,8 @@ resource "aws_subnet" "public" {
 
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"  # Replace with your desired private subnet CIDR block
-  availability_zone = "us-west-2a"  # Replace with your desired availability zone
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-west-2a"
   tags = {
     Name = "PrivateSubnet"
   }
